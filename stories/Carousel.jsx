1c9b2f98d2aba5d2c33a79613dedd3093faa0730
7mioda <ahmededaly1993@gmail.com>
2019-04-23 18:15:46 +0200

    carousel-feature

--- stories/Carousel.jsx
+++ stories/Carousel.jsx
@@ -4,7 +4,18 @@ import Carousel from '../components/Carousel/Carousel';
 
 storiesOf('Carousel', module)
   .add('Carousel with Images', () => (
-    <Carousel height="400" width="1024">
+    <Carousel height="400" auto indicators width="1024">
+      <img src="https://picsum.photos/420/800" alt="" />
+      <img src="https://picsum.photos/800/800" alt="" />
+      <img src="https://picsum.photos/500/800" alt="" />
+      <img src="https://picsum.photos/600/800" alt="" />
+      <img src="https://picsum.photos/600/800" alt="" />
+      <img src="https://picsum.photos/600/800" alt="" />
+      <img src="https://picsum.photos/600/800" alt="" />
+    </Carousel>
+  ))
+  .add('Rectangular Carousel with Images', () => (
+    <Carousel height="400" width="400">
       <img src="https://picsum.photos/420/800" alt="" />
       <img src="https://picsum.photos/800/800" alt="" />
       <img src="https://picsum.photos/500/800" alt="" />
@@ -12,8 +23,8 @@ storiesOf('Carousel', module)
       <img src="https://picsum.photos/600/800" alt="" />
     </Carousel>
   ))
-  .add('Rectangular Carousel with Images', () => (
-    <Carousel height="400" width="400">
+  .add('Rectangular Carousel with auto Slide Images', () => (
+    <Carousel auto indicators height="400" width="400">
       <img src="https://picsum.photos/420/800" alt="" />
       <img src="https://picsum.photos/800/800" alt="" />
       <img src="https://picsum.photos/500/800" alt="" />
